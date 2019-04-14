91561948eec8775d9fe7a19fe7fd45eae6a0b5e6
Mike <mrg14r@gmail.com>
2019-04-14 14:57:43 +1000

    remove unused components

--- src/components/Button/Button.stories.tsx
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-import { storiesOf } from '@storybook/react-native';
-import Button from './Button';
-
-storiesOf('Button', module).add('default', () => <Button>Hello</Button>);
